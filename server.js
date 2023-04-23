342f2a0daaf79082ff04fdbf5d54dde22a9be5ad
Anna Zaldivar <anna.zaldivar@gmail.com>
2023-04-23 11:44:47 -0700

    Initial working server

---
+++ server.js
@@ -0,0 +1,19 @@
+
+const express = require("express");
+
+// CONFIGURATION
+require("dotenv").config();
+const PORT = process.env.PORT;
+console.log ("My port is:", PORT);
+
+const app= express ();
+
+//ROUTES
+app.get("/", (req,res) => {
+    res.send("Welcome to an Awesome App about Breads");
+});
+
+app.listen(PORT, () => {
+    console.log("Server is listening on port", PORT);
+});
+
