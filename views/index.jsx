8f138c4f39bc4041376a9be82f36c14336f4d293
Anna Zaldivar <anna.zaldivar@gmail.com>
2023-04-23 14:02:41 -0700

    href

---
+++ views/index.jsx
@@ -0,0 +1,26 @@
+const React = require("react");
+const Default = require ("./layouts/default.jsx");
+
+
+
+const Index = ({breads}) => {
+    return (
+    <Default>
+        <h2>Index Page</h2>
+        <ul>
+            {breads.map((bread, index) => (
+               <li key= {index}>
+                <a href= {`/breads/${index}`}>{bread.name}</a>
+               </li>
+               
+            ))}
+           {/*<li>Rye <li>
+              <li>Wheat<li>
+              <li>...<li> */} 
+        </ul>
+    </Default>
+    );
+};
+
+module.exports = Index;
+
